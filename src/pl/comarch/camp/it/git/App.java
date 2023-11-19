6d38661631d7187786300a14cfdba214eb4cb569
Mateusz <bmatteo@interia.pl>
2023-11-19 15:20:52 +0100

    update

--- src/pl/comarch/camp/it/git/App.java
+++ src/pl/comarch/camp/it/git/App.java
@@ -3,5 +3,7 @@ package pl.comarch.camp.it.git;
 public class App {
     public static void main(String[] args) {
         System.out.println("cos");
+
+        System.out.println("cos nowego");
     }
 }
